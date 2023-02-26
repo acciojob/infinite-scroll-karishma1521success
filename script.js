376e5675f17dd6cc33965d2b9ff62efd4401e3fb
karishma1521success <105378376+karishma1521success@users.noreply.github.com>
2023-02-26 23:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
+let ol = document.getElementById('infi-list');
+let n = 10;
 
+function addLiOnScrolling(){
+	alert("heuuu");
+	let li = document.createElement('li');
+	li.textContent = 'Item '+ (n+1);
+	ol.appendChild(li);
+}
+
+window.addEventListener('scroll' , addLiOnScrolling);
